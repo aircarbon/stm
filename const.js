1db166e3fd4ac1ecd80f15ffc1e514e4f277f9c7
dom <dom@lamoto.co.uk>
2019-08-04 23:31:11 +0800

    multiple config contexts

--- const.js
+++ const.js
@@ -1,13 +1,11 @@
-const config = require('./config.js');
-
 module.exports = {
     eeuType: Object.freeze({
         UNFCCC: 0,
         VCS: 1,
     }),
 
-    tonCarbon: 1000, // one ton carbon in kg
-    ktCarbon: 1000 * 1000, // kiloton carbon in kg
-    mtCarbon: 1000 * 1000 * 1000, // megaton carbon in kg
+    tonCarbon: 1000,                     // one ton carbon in kg
+    ktCarbon: 1000 * 1000,               // kiloton carbon in kg
+    mtCarbon: 1000 * 1000 * 1000,        // megaton carbon in kg
     gtCarbon: 1000 * 1000 * 1000 * 1000, // gigaton carbon in kg
 };
