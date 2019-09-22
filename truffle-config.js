bc9d49d14c11d4e5cad6ecacc84a954cfc2e7418
Kaka Huynh <dunghd.it@gmail.com>
2019-09-22 09:44:43 +0900

    fix(core): downgrade web3 to alpha for fixing the nonce issue
    All packages will be use the same web3 version

--- truffle-config.js
+++ truffle-config.js
@@ -17,124 +17,139 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-const Web3 = require('web3');
+const Web3 = require("web3");
 const web3 = new Web3();
 
-var HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 // wallet seed (only needed for public testnets: ropsten & rinkeby - manage wallet with MetaMask)
-const rootPackageJson = require('../../package.json');
+const rootPackageJson = require("../../package.json");
 const mnemonic = rootPackageJson.config.mnemonic;
 
 module.exports = {
-    /**
-     * Networks define how you connect to your ethereum client and let you set the
-     * defaults web3 uses to send transactions. If you don't specify one truffle
-     * will spin up a development blockchain for you on port 9545 when you
-     * run `develop` or `test`. You can ask a truffle command to use a specific
-     * network from the command line, e.g
-     *
-     * $ truffle test --network <network-name>
-     */
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
 
-    networks: {
-        solc: {
-            optimizer: {
-                enabled: true,
-                runs: 100,
-            },
-        },
-
-        // Useful for testing. The `development` name is special - truffle uses it by default
-        // if it's defined here and no other network is specified at the command line.
-        // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-        // tab if you use this network and you must also set the `host`, `port` and `network_id`
-        // options below to some value.
-        //
-        // development: { // for "truffle develop"  (built-in ganache, max 10 accounts)
-        //     host: "127.0.0.1",
-        //     port: 9545,
-        //     network_id: "*",
-        // },
-        development: {
-            // for "truffle test" -- with manually launched "ganache-cli -a 200" (1000 test accounts)
-            host: '127.0.0.1',
-            port: 8545,
-            network_id: '*',
-        },
-
-        // aircarbon geth node -- a bit faster than infura, but representative of mainnet
-        ropsten_ac: {
-            //provider:() =>  new HDWalletProvider(mnemonic, 'https://node0.scoop.tech:9545', 0, 200),
-            provider: () => new HDWalletProvider(mnemonic, 'https://ac-dev0.net:9545', 0, 200),
-            network_id: '*',
-            gas: 6000000,
-            gasPrice: web3.utils.toWei('40', 'gwei'),
-        },
-
-        // ropsten infura -- much slower than rinkeby infura
-        ropsten_infura: {
-            provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/93db2c7fd899496d8400e86100058297", 0, 200),
-            network_id: "*",
-            gas: 6000000,
-            gasPrice: web3.utils.toWei("40", "gwei"),
-        },
-
-        rinkeby_infura: {
-            //provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/93db2c7fd899496d8400e86100058297', 0, 200),
-            provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/93db2c7fd899496d8400e86100058297', 0, 200),
-            network_id: '*',
-            gas: 6000000,
-            gasPrice: web3.utils.toWei('40', 'gwei'),
-        },
-
-        // Another network with more advanced options...
-        // advanced: {
-        // port: 8777,             // Custom port
-        // network_id: 1342,       // Custom network
-        // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-        // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-        // from: <address>,        // Account to send txs from (default: accounts[0])
-        // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-        // },
-
-        // Useful for deploying to a public network.
-        // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
-
-        // Useful for private networks
-        // private: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-        // network_id: 2111,   // This network is yours, in the cloud.
-        // production: true    // Treats this network as if it was a public net. (default: false)
-        // }
+  networks: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 100
+      }
     },
 
-    // Set default mocha options here, use special reporters etc.
-    mocha: {
-        timeout: 0,
-        enableTimeouts: false,
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: { // for "truffle develop"  (built-in ganache, max 10 accounts)
+    //     host: "127.0.0.1",
+    //     port: 9545,
+    //     network_id: "*",
+    // },
+    development: {
+      // for "truffle test" -- with manually launched "ganache-cli -a 200" (1000 test accounts)
+      host: process.env.GANACHE_HOST || "127.0.0.1",
+      port: 8545,
+      gas: 6000000,
+      network_id: "*"
     },
 
-    // Configure your compilers
-    compilers: {
-        solc: {
-            version: "0.5.8",    // Fetch exact version from solc-bin (default: truffle's version)
-            docker: false,       // Use "0.5.8" you've installed locally with docker (default: false)
-            settings: {          // See the solidity docs for advice about optimization and evmVersion
-                optimizer: {
-                    enabled: true,
-                    runs: 100
-                },
-                evmVersion: "byzantium"
-            }
-        },
+    // aircarbon geth node -- a bit faster than infura, but representative of mainnet
+    ropsten_ac: {
+      //provider:() =>  new HDWalletProvider(mnemonic, 'https://node0.scoop.tech:9545', 0, 200),
+      provider: () =>
+        new HDWalletProvider(mnemonic, "https://ac-dev0.net:9545", 0, 200),
+      network_id: "*",
+      gas: 6000000,
+      gasPrice: web3.utils.toWei("40", "gwei")
     },
+
+    // ropsten infura -- much slower than rinkeby infura
+    ropsten_infura: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/93db2c7fd899496d8400e86100058297",
+          0,
+          200
+        ),
+      network_id: "*",
+      gas: 6000000,
+      gasPrice: web3.utils.toWei("40", "gwei")
+    },
+
+    rinkeby_infura: {
+      //provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/93db2c7fd899496d8400e86100058297', 0, 200),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/93db2c7fd899496d8400e86100058297",
+          0,
+          200
+        ),
+      network_id: "*",
+      gas: 6000000,
+      gasPrice: web3.utils.toWei("40", "gwei")
+    }
+
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    // ropsten: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
+
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 0,
+    enableTimeouts: false
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.8", // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false, // Use "0.5.8" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 100
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  }
 };
