c59663657e600ecca2e68ccfb3062ef2f221dcb6
Kaka Huynh <dunghd.it@gmail.com>
2019-11-27 23:47:32 +0700

    refactor(erc20): support ganache network id from env file

---
+++ ganache.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const shell = require('shelljs');
+
+const PORT = 8545;
+const TOTAL_ACCOUNTS = 888;
+const { NETWORK_ID } = process.env;
+const MNEMONIC = 'educate school blast ability display bleak club soon curve car oil ostrich';
+const command = `ganache-cli -p ${PORT} -a ${TOTAL_ACCOUNTS} --networkId ${NETWORK_ID} -m "${MNEMONIC}"`;
+
+shell.echo(`Run: ${command}`);
+
+if (shell.exec(command).code !== 0) {
+  shell.echo('Error: ganache-cli failed');
+  shell.exit(1);
+}
