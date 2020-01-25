66ffef2a555b5c7d2aacdfd7810413e292f36926
dom <dom@lamoto.co.uk>
2020-01-25 06:27:45 +0000

    sol: fix bip39 24 word test

--- test/05_Test_Misc.js
+++ test/05_Test_Misc.js
@@ -28,9 +28,9 @@ contract("StMaster", accounts => {
     it(`misc - bip39 - should be able to use a maximum entropy (24 word = 512-bit) BIP39 mnemonic (BIP44 HD)`, async () => {
         for (let i=0 ; i < 10 ; i++) {
             const x = await CONST.getAccountAndKey(i, 'trick clog bounce style early business emotion sun piece divide office fiscal attract betray virus salute cannon test blood stick nose reform turkey oval');
-            console.log(`${i} addr: ${x.addr} privKey: ${x.privKey}`);
+            //console.log(`${i} addr: ${x.addr} privKey: ${x.privKey}`);
             if (i==0) {
-                assert(x.privKey == '0x4e4dfa0683f3460113343f236b600ab816df8773b6749b420e3a07d0ff7aaf28')
+                assert(x.privKey == '4e4dfa0683f3460113343f236b600ab816df8773b6749b420e3a07d0ff7aaf28', 'unexpected key')
             }
         }
     });
