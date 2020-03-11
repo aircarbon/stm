78eb82d47afb804dbbccb1c30ba74ba3c7661249
Huynh Duc Dung <dung@aircarbon.co>
2020-03-11 16:35:17 +0800

    test(ex-api): end2end for deposit fiat endpoint

--- seal.js
+++ seal.js
@@ -1,26 +1,25 @@
 require("dotenv").config();
-const CONST = require('./const.js');
+const { getAccountAndKey, web3_call, web3_tx } = require('./const.js');
 
 process.env.WEB3_NETWORK_ID = Number(process.env.NETWORK_ID || 888);
 
 (async function() {
-  var x;
-  x = await CONST.getAccountAndKey(0);
+  let x = await getAccountAndKey(0);
   OWNER = x.addr; OWNER_privKey = x.privKey;
 
-  const contractSealed = await CONST.web3_call('getContractSeal', []);
+  const contractSealed = await web3_call('getContractSeal', []);
   console.log('contractSealed: ', contractSealed);
   if (!contractSealed) {
-    const WHITELIST_COUNT = 20;
+    const WHITELIST_COUNT = 50;
     console.group('WHITELISTING...');
-    for (var i=0 ; i < WHITELIST_COUNT ; i++) { // note - we include account[0] owner account in the whitelist
-      x = await CONST.getAccountAndKey(i);
-      const whitelistTx = await CONST.web3_tx('whitelist', [ x.addr ], OWNER, OWNER_privKey);
+    for (let i=0 ; i < WHITELIST_COUNT ; i++) { // note - we include account[0] owner account in the whitelist
+      x = await getAccountAndKey(i);
+      await web3_tx('whitelist', [ x.addr ], OWNER, OWNER_privKey);
     }
     console.groupEnd();
 
     console.group('SEALING...');
-    const sealTx = await CONST.web3_tx('sealContract', [], OWNER, OWNER_privKey);
+    await web3_tx('sealContract', [], OWNER, OWNER_privKey);
     console.groupEnd();
   }
 
