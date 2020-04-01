1993cdcd74e1ed43f2f7cab300781c1b7539e011
Huynh Duc Dung <dung@aircarbon.co>
2020-04-01 17:45:41 +0800

    feat(erc20): save whitelist addresses to db

--- seal.js
+++ seal.js
@@ -1,20 +1,22 @@
-require("dotenv").config();
+require('dotenv').config();
 const { getAccountAndKey, web3_call, web3_tx } = require('./const.js');
 
 process.env.WEB3_NETWORK_ID = Number(process.env.NETWORK_ID || 888);
 
-(async function() {
+(async function () {
   let x = await getAccountAndKey(0);
-  OWNER = x.addr; OWNER_privKey = x.privKey;
+  OWNER = x.addr;
+  OWNER_privKey = x.privKey;
 
   const contractSealed = await web3_call('getContractSeal', []);
   console.log('contractSealed: ', contractSealed);
   if (!contractSealed) {
-    const WHITELIST_COUNT = Number(process.env.WHITELIST_COUNT || 30) ;
+    const WHITELIST_COUNT = Number(process.env.WHITELIST_COUNT || 30);
     console.group('WHITELISTING...');
-    for (let i=0 ; i < WHITELIST_COUNT ; i++) { // note - we include account[0] owner account in the whitelist
+    for (let i = 0; i < WHITELIST_COUNT; i++) {
+      // note - we include account[0] owner account in the whitelist
       x = await getAccountAndKey(i);
-      await web3_tx('whitelist', [ x.addr ], OWNER, OWNER_privKey);
+      await web3_tx('whitelist', [x.addr], OWNER, OWNER_privKey);
     }
     console.groupEnd();
 
