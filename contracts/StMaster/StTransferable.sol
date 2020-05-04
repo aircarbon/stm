b9bf6f72f4139da09eadb623b98b4fc017191aec
dom <dom@lamoto.co.uk>
2020-05-04 09:14:37 +0800

    cleanup: misc

--- contracts/StMaster/StTransferable.sol
+++ contracts/StMaster/StTransferable.sol
@@ -40,7 +40,7 @@ contract StTransferable is Owned,
     }
 
     // SLOW - fee preview (full, slow) - old/deprecate?
-    // 24k - remove
+    // 24k - remove?
     uint256 constant MAX_BATCHES_PREVIEW = 128; // library constants not accessible in contract; must duplicate TransferLib value
     function transfer_feePreview(StructLib.TransferArgs calldata a)
     external view returns (StructLib.FeesCalc[1 + MAX_BATCHES_PREVIEW * 2] memory feesAll) {
