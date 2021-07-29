5daffcbcd177ede5b93af6dfc30657b8bc80cd96
dom <dom@lamoto.co.uk>
2021-07-29 06:09:17 +0100

    rec

--- sol/test_web3/200_Web3_Reconcile.js
+++ sol/test_web3/200_Web3_Reconcile.js
@@ -33,55 +33,70 @@ describe(`Contract Web3 Interface`, async () => {
     console.log("types", types.map(p => p.name).join(', '));
 
     // (A) iterate LEs, get STs from all LEs
-    const ledgerOwners = await CONST.web3_call("getLedgerOwners", []);
-    console.group("A");
-    var tot_minted_A = 0;
-    var tot_countST_A = 0;
-    for (var ledgerOwner of ledgerOwners) {
-        const ledgerEntry = await CONST.web3_call("getLedgerEntry", [ledgerOwner]);
-        const ccyInfo = [], tokInfo = [];
-        for (var ccy of ledgerEntry.ccys) {
-            ccyInfo.push(`name:${ccy.name} bal:${ccy.balance.toString()}`);
-        }
-        for (var token of ledgerEntry.tokens) {
-            const bn = new BN(token.stId.toString());
-            tokInfo.push(chalk.dim(`tokTypeId:${token.tokTypeId} batchId:${token.batchId} stId:0x${bn.toString(16)} currentQty:${token.currentQty} mintedQty:${token.mintedQty}`));
-            tot_minted_A += Number(token.mintedQty);
-            tot_countST_A++;
-        }
-        console.log(`${ledgerOwner} ${ccyInfo.join(" ")}\n\t${tokInfo.join("\n\t")}`);
-    }
-    console.log(`ledgerOwners.length: ${ledgerOwners.length}`); // prod: 41
-    console.log(`tot_countST_A: ${tot_countST_A}`); // prod: 77
-    console.log(`tot_minted_A: ${tot_minted_A}`); // prod: 1115266000
-    console.log(`getSecToken_totalMintedQty: ${(await await CONST.web3_call("getSecToken_totalMintedQty", []))}`); // prod: 1698725000
-    console.groupEnd("A");
+    // const ledgerOwners = await CONST.web3_call("getLedgerOwners", []);
+    // console.group("A");
+    // var tot_minted_A = 0, tot_current_A = 0, tot_countST_A = 0;
+    // for (var ledgerOwner of ledgerOwners) {
+    //     const ledgerEntry = await CONST.web3_call("getLedgerEntry", [ledgerOwner]);
+    //     const ccyInfo = [], tokInfo = [];
+    //     for (var ccy of ledgerEntry.ccys) {
+    //         ccyInfo.push(`name:${ccy.name} bal:${ccy.balance.toString()}`);
+    //     }
+    //     for (var token of ledgerEntry.tokens) {
+    //         const bn = new BN(token.stId.toString());
+    //         tokInfo.push(chalk.dim(`tokTypeId:${token.tokTypeId} batchId:${token.batchId} stId:0x${bn.toString(16)} currentQty:${token.currentQty} mintedQty:${token.mintedQty}`));
+    //         tot_minted_A += Number(token.mintedQty);
+    //         tot_current_A += Number(token.currentQty);
+    //         tot_countST_A++;
+    //     }
+    //     console.log(`${ledgerOwner} ${ccyInfo.join(" ")}\n\t${tokInfo.join("\n\t")}`);
+    // }
+    // console.log(`ledgerOwners.length: ${ledgerOwners.length}`); // prod: 41
+    // console.log(`tot_countST_A: ${tot_countST_A}`); // prod: 77
+    // console.log(`tot_minted_A: ${tot_minted_A}`); // prod: 1115266000
+    // console.log(`tot_current_A: ${tot_minted_A}`); // prod: ...
+    // console.log(`getSecToken_totalMintedQty: ${(await await CONST.web3_call("getSecToken_totalMintedQty", []))}`); // prod: 1698725000
+    // console.groupEnd("A");
 
-    // (2) iterate STs directly
+    // (2) iterate STs directly - TODO: x-ref w/ batch totals, to see which batch/STs have the issue (### 10% discrepancy)
     console.group("B");
     const baseStId = (await CONST.web3_call("getSecToken_BaseId", [])).toString(10);
     const maxStId = (await CONST.web3_call("getSecToken_MaxId", [])).toString(10);
     console.log("baseStId", baseStId);
     console.log("maxStId", maxStId);
+    const sts = [];
     var tot_minted_B = 0;
     var tot_countST_B = 0;
     for (var i = baseStId ; i <= maxStId ; i++) {
         const token = (await CONST.web3_call("getSecToken", [i]));
         const bn = new BN(token.stId.toString());
-        if (Number(token.currentQty) != Number(token.mintedQty)) {
+        //if (Number(token.currentQty) != Number(token.mintedQty)) {
             console.log(chalk.dim(`tokTypeId:${token.tokTypeId} batchId:${token.batchId} stId:0x${bn.toString(16)} currentQty:${token.currentQty} mintedQty:${token.mintedQty}`));
-        }
+        //}
         tot_minted_B += Number(token.mintedQty);
         tot_countST_B++;
+        sts.push(token);
     }
     console.log(`tot_countST_B: ${tot_countST_B}`); // 96
     console.log(`tot_minted_B: ${tot_minted_B}`); // prod: 1817787000
     console.log(`getSecToken_totalMintedQty: ${(await await CONST.web3_call("getSecToken_totalMintedQty", []))}`); // prod: 1698725000
+
+    // (3) x-ref w/ batches...
+    const maxBatchId = await CONST.web3_call("getSecTokenBatch_MaxId", []);
+    const batches = [];
+    console.log("maxBatchId", maxBatchId);
+    for (var i = 1; i <= maxBatchId ; i++) {
+        const batch = await CONST.web3_call("getSecTokenBatch", [i]);
+        const sts_forBatch = sts.filter(p => p.batchId == i);
+        const sts_totMinted_forBatch = sts_forBatch.map(p => Number(p.mintedQty)).reduce((a,b) => a + b, 0);
+        console.log(`got batchid=${i} mintedQty=${batch.mintedQty} #sts_forBatch=${sts_forBatch.length} sts_totMinted_forBatch=${sts_totMinted_forBatch}`);
+        batches.push(batch);
+    }
     console.groupEnd("B");
 
     // ... when burning a full ST: it doesn't get removed from the _sts[] list (only from the ledger)
-    // (hence difference between tot_countST_B && tot_countST_A?)
-    // i.e. the consistency check fail (1) is INVALID: ledger ST minted !== spot_totalMinted
+    // (hence difference between tot_countST_B && tot_countST_A?) 
+    // #### i.e. the consistency check fail (1) is INVALID: ledger ST minted !== spot_totalMinted ####
     //   (the latter is all time, the former is current)
 
     // that still leaves discrepancy between all STs across all time !== _spot_totalMintedQty (it should be)
