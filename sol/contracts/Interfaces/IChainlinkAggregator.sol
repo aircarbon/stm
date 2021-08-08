39e4304902db3a0e5d683bd30f80157ca473e9e0
Ankur <ankur.daharwal@gmail.com>
2021-08-08 18:36:05 +0800

    🐛 fix(stm): revert chainlink xface to old version

--- sol/contracts/Interfaces/IChainlinkAggregator.sol
+++ sol/contracts/Interfaces/IChainlinkAggregator.sol
@@ -1,6 +1,23 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-only
 // Author: @ankurdaharwal
 pragma solidity ^0.8.0;
+
+/**
+ * @notice Chainlink Reference Data Contract
+ * @dev https://docs.chain.link/docs/using-chainlink-reference-contracts
+ */
+ interface IChainlinkAggregator {
+  function latestAnswer() external view returns (int256);
+  function latestTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+  function getAnswer(uint256 roundId) external view returns (int256);
+  function getTimestamp(uint256 roundId) external view returns (uint256);
+
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
+  event NewRound(uint256 indexed roundId, address indexed startedBy);
+}
+
+// TODO: ChainLink Aggregator V3 Version
 
 /**
  * @notice Chainlink Reference Data Contract
@@ -11,6 +28,7 @@ pragma solidity ^0.8.0;
 // Certik: (Major) ICA-01 | Incorrect Chainlink Interface
 // Resolved: (Major) ICA-01 | Upgraded Chainlink Aggregator Interface to V3
 
+/*
 interface IChainlinkAggregator {
   function decimals() external view returns (uint8);
   function description() external view returns (string memory);
@@ -35,3 +53,4 @@ interface IChainlinkAggregator {
       uint80 answeredInRound
     );
 }
+*/
