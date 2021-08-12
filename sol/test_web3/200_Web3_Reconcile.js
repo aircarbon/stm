b1998539416e46644fc698a0b02e592c82d87ef0
DM <dom@d0m1.com>
2021-08-12 11:12:37 +0100

    WIP: reconciliation notes

--- sol/test_web3/200_Web3_Reconcile.js
+++ sol/test_web3/200_Web3_Reconcile.js
@@ -36,30 +36,30 @@ describe(`Contract Web3 Interface`, async () => {
     console.log("types", types.map(p => p.name).join(', '));
 
     // (A) iterate LEs, get STs from all LEs
-    // const ledgerOwners = await CONST.web3_call("getLedgerOwners", []);
-    // console.group("A");
-    // var tot_minted_A = 0, tot_current_A = 0, tot_countST_A = 0;
-    // for (var ledgerOwner of ledgerOwners) {
-    //     const ledgerEntry = await CONST.web3_call("getLedgerEntry", [ledgerOwner]);
-    //     const ccyInfo = [], tokInfo = [];
-    //     for (var ccy of ledgerEntry.ccys) {
-    //         ccyInfo.push(`name:${ccy.name} bal:${ccy.balance.toString()}`);
-    //     }
-    //     for (var token of ledgerEntry.tokens) {
-    //         const bn = new BN(token.stId.toString());
-    //         tokInfo.push(chalk.dim(`tokTypeId:${token.tokTypeId} batchId:${token.batchId} stId:0x${bn.toString(16)} currentQty:${token.currentQty} mintedQty:${token.mintedQty}`));
-    //         tot_minted_A += Number(token.mintedQty);
-    //         tot_current_A += Number(token.currentQty);
-    //         tot_countST_A++;
-    //     }
-    //     console.log(`${ledgerOwner} ${ccyInfo.join(" ")}\n\t${tokInfo.join("\n\t")}`);
-    // }
-    // console.log(`ledgerOwners.length: ${ledgerOwners.length}`); // prod: 41
-    // console.log(`tot_countST_A: ${tot_countST_A}`); // prod: 77
-    // console.log(`tot_minted_A: ${tot_minted_A}`); // prod: 1115266000
-    // console.log(`tot_current_A: ${tot_minted_A}`); // prod: ...
-    // console.log(`getSecToken_totalMintedQty: ${(await await CONST.web3_call("getSecToken_totalMintedQty", []))}`); // prod: 1698725000
-    // console.groupEnd("A");
+    const ledgerOwners = await CONST.web3_call("getLedgerOwners", []);
+    console.group("A");
+    var tot_minted_A = 0, tot_current_A = 0, tot_countST_A = 0;
+    for (var ledgerOwner of ledgerOwners) {
+        const ledgerEntry = await CONST.web3_call("getLedgerEntry", [ledgerOwner]);
+        const ccyInfo = [], tokInfo = [];
+        for (var ccy of ledgerEntry.ccys) {
+            ccyInfo.push(`name:${ccy.name} bal:${ccy.balance.toString()}`);
+        }
+        for (var token of ledgerEntry.tokens) {
+            const bn = new BN(token.stId.toString());
+            tokInfo.push(chalk.dim(`tokTypeId:${token.tokTypeId} batchId:${token.batchId} stId:0x${bn.toString(16)} currentQty:${token.currentQty} mintedQty:${token.mintedQty}`));
+            tot_minted_A += Number(token.mintedQty);
+            tot_current_A += Number(token.currentQty);
+            tot_countST_A++;
+        }
+        console.log(`${ledgerOwner} ${ccyInfo.join(" ")}\n\t${tokInfo.join("\n\t")}`);
+    }
+    console.log(`ledgerOwners.length: ${ledgerOwners.length}`); // prod: 41
+    console.log(`tot_countST_A: ${tot_countST_A}`); // prod: 77
+    console.log(`tot_minted_A: ${tot_minted_A}`); // prod: 1115266000
+    console.log(`tot_current_A: ${tot_minted_A}`); // prod: ...
+    console.log(`getSecToken_totalMintedQty: ${(await await CONST.web3_call("getSecToken_totalMintedQty", []))}`); // prod: 1698725000
+    console.groupEnd("A");
 
     // (2) iterate STs directly - TODO: x-ref w/ batch totals, to see which batch/STs have the issue (### 10% discrepancy)
     console.group("B");
@@ -99,11 +99,8 @@ describe(`Contract Web3 Interface`, async () => {
 
     // ... when burning a full ST: it doesn't get removed from the _sts[] list (only from the ledger)
     // (hence difference between tot_countST_B && tot_countST_A?) 
-    // #### i.e. the consistency check fail (1) is INVALID: ledger ST minted !== spot_totalMinted ####
-    //   (the latter is all time, the former is current)
+    // #### i.e. the consistency check fail (1) is STILL UNACOUNTED FOR?!
 
     // that still leaves discrepancy between all STs across all time !== _spot_totalMintedQty (it should be)
-
-    // separately: check that the 77 "live" (non-burned) STs for the 41 ledger entries === the actual physical amount held
   });
 });
