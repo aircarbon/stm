85f7b3dff96e2711ab28a1de1398615979ea69f7
Vicente Linero <vlinero@gmail.com>
2021-10-27 16:49:16 +0800

    Revert "Remove dev_mnemonic"
    This reverts commit 67c4dddfc3b0200ab389c3e9253c010906f4ff6f.

---
+++ sol/DEV_MNEMONIC.js
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-only - (c) AirCarbon Pte Ltd - see /LICENSE.md for Terms
+
+module.exports = {
+  MNEMONIC: 'educate school blast ability display bleak club soon curve car oil ostrich',
+};
